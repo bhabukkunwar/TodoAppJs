0d3800e0227fa1984c988fda1dd0e691f25b7668
bhabuk <bhabukunwar@gmail.com>
2022-01-31 22:45:45 +0545

    correct

--- toDoApp/App.js
+++ toDoApp/App.js
@@ -6,42 +6,43 @@ const form = document.querySelector('form')
 
 const appDiv = document.querySelector('appDiv')
 
-// creating message to show the status of tasks
-const message = document.createElement('li')
-const text = document.createTextNode('No Tasks Yet!')
-message.append(text)
-message.classList.add('message')
+// // creating message to show the status of tasks
+// // const message = document.createElement('li')
+// // const messageText = document.createTextNode('No Tasks Yet!')
+// // message.append(messageText)
+// // message.classList.add('message')
+// // message.style.marginTop = '5rem'
+// // message.style.color = '5rem'
 
-form.appendChild(taskStatus)
+// form.appendChild(message)
 
 submit.addEventListener('click', function (e) {
   e.preventDefault()
+
   addTask(task)
 })
 
 function addTask(task) {
+  // form.removeChild(message)
   // creating elements dynamically
   const ulist = document.createElement('ul')
   const list = document.createElement('li')
   const text = document.createTextNode(task.value)
-  const remover = document.createElement('button')
-
-  // assigning class to remove button
-  remover.classList.add('btn')
-  remover.textContent = 'X'
-
-  // appending elemets dynamically
-  list.appendChild(remover)
 
   list.classList.add('list')
+  ulist.classList.add('ul')
 
   list.append(text)
   ulist.append(list)
-  form.appendChild(ulist)
+  form.append(ulist)
 
-  // adding event listener on remove button
-  remover.addEventListener('click', function (e) {
+  // changging the color of taks when on mouse hover
+  list.addEventListener('mouseover', function (e) {
     e.preventDefault()
-    ulist.remove(list)
+    list.classList.add('colorchange')
+  })
+  list.addEventListener('mouseout', function (e) {
+    e.preventDefault()
+    list.classList.remove('colorchange')
   })
 }
